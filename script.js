9fed40c76a55179342dc10a12d8622478b03000d
Heisenburn <marcinrudnik1996@gmail.com>
2020-07-01 11:26:06 +0200

    first upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#servicesCarrousel').slick({
+        
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+      });
+                  
+  });

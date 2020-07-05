f46ffc67a9e5114f7d6d88a79a173019d444b69a
Heisenburn <marcinrudnik1996@gmail.com>
2020-07-05 19:21:30 +0200

    scrollTo.js

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
     $('#servicesCarrousel').slick({
-        
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
+    
         infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+     
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+     
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',
+        
+      
       });
-                  
+ 
+
+      var coll = document.getElementsByClassName("collapsible");
+      var i;
+
+       console.log(coll)
+      
+      for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+        });
+      }
+      
   });
+
